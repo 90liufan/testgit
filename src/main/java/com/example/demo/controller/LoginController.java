23afe7a39b149e0eb9e3c169e361d9499286207f
testgit <testgit>
2022-05-22 10:52:10 +0800

    git学习测试

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -16,4 +16,38 @@ public class LoginController {
         return name;
     }
 
+    @GetMapping(value="lambda")
+    public String testLambda(@RequestParam String name){
+        Java8Tester tester = new Java8Tester();
+
+        // 类型声明
+        com.example.demo.controller.MathOperation addition = (int a, int b) -> a + b;
+
+        // 不用类型声明
+        com.example.demo.controller.MathOperation subtraction = (a, b) -> a - b;
+
+        // 大括号中的返回语句
+        com.example.demo.controller.MathOperation multiplication = (int a, int b) -> { return a * b; };
+
+        // 没有大括号及返回语句
+        com.example.demo.controller.MathOperation division = (int a, int b) -> a / b;
+
+        System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
+        System.out.println("10 - 5 = " + tester.operate(10, 5, subtraction));
+        System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
+        System.out.println("10 / 5 = " + tester.operate(10, 5, division));
+
+        // 不用括号
+        com.example.demo.controller.GreetingService greetService1 = message ->
+                System.out.println("Hello " + message);
+
+        // 用括号
+        com.example.demo.controller.GreetingService greetService2 = (message) ->
+                System.out.println("Hello " + message);
+
+        greetService1.sayMessage("Runoob");
+        greetService2.sayMessage("Google");
+        return name;
+    }
+
 }
