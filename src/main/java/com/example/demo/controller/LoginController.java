a01803ac0a14947625c4b554f18a5e513530707a
testgit <testgit>
2022-05-22 10:30:03 +0800

    git学习测试

---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping(value="/get")
+public class LoginController {
+
+    @GetMapping(value="mytest")
+    public String getRequest(@RequestParam String name){
+        return name;
+    }
+
+}
