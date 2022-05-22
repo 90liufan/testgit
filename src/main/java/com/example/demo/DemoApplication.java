4158dd18cb4e04d6735a972cd62e2b3d5430ac11
testgit <testgit>
2022-05-22 09:57:51 +0800

    init初始化

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		  SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
