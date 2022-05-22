552a330bb554180a898fbf85dc98481504d7725c
testgit <testgit>
2022-05-22 16:35:46 +0800

    git学习测试

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.DefaultService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,12 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class LoginController {
 
+    @Autowired
+    private DefaultService defaultService;
     @GetMapping(value="mytest")
     public String getRequest(@RequestParam String name){
         log.info("这是第一个测试"+name);
         log.info("这是第二个测试"+name);
         return name;
 
+    }
+    @GetMapping(value="lambda")
+    public String testLambda(){
+        defaultService.testDefault();
+        return "success";
     }
 
     @GetMapping(value="lambda")
---
+++ src/main/java/com/example/demo/service/DefaultService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public interface DefaultService {
+    default void testDefault(){
+        System.out.println("this is test");
+    }
+}
