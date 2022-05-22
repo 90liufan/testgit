6d3503c10b2447702ed9b6fdde2f2535d2c3673d
testgit <testgit>
2022-05-22 10:38:37 +0800

    git学习测试

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,15 +1,18 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/get")
+@Slf4j
 public class LoginController {
 
     @GetMapping(value="mytest")
     public String getRequest(@RequestParam String name){
+        log.info("这是第一个测试"+name);
         return name;
     }
 
