aceb5018b9ad3af3a80a0b71bf12b8d2688f09d2
testgit <testgit>
2022-05-24 22:51:21 +0800

    "git学习测试"

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -44,38 +43,6 @@ public class LoginController {
         return "success";
     }
 
-    @GetMapping(value="lambda")
-    public String testLambda(@RequestParam String name){
-        Java8Tester tester = new Java8Tester();
 
-        // 类型声明
-        com.example.demo.controller.MathOperation addition = (int a, int b) -> a + b;
-
-        // 不用类型声明
-        com.example.demo.controller.MathOperation subtraction = (a, b) -> a - b;
-
-        // 大括号中的返回语句
-        com.example.demo.controller.MathOperation multiplication = (int a, int b) -> { return a * b; };
-
-        // 没有大括号及返回语句
-        com.example.demo.controller.MathOperation division = (int a, int b) -> a / b;
-
-        System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
-        System.out.println("10 - 5 = " + tester.operate(10, 5, subtraction));
-        System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
-        System.out.println("10 / 5 = " + tester.operate(10, 5, division));
-
-        // 不用括号
-        com.example.demo.controller.GreetingService greetService1 = message ->
-                System.out.println("Hello " + message);
-
-        // 用括号
-        com.example.demo.controller.GreetingService greetService2 = (message) ->
-                System.out.println("Hello " + message);
-
-        greetService1.sayMessage("Runoob");
-        greetService2.sayMessage("Google");
-        return name;
-    }
 
 }
