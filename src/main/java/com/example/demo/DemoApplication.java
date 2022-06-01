af9871e2f6602908031a571d1842367b709036d0
testgit <testgit>
2022-06-01 16:49:09 +0800

    git学习测试

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("--------");
 		  SpringApplication.run(DemoApplication.class, args);
 	}
 
