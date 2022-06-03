01266638fd379a2f6f8a0fde5c8f4324a729d4c2
testgit <testgit>
2022-06-03 21:08:28 +0800

    git学习测试

---
+++ src/main/java/com/example/demo/mapper/StudentMapper.java
@@ -0,0 +1,8 @@
+package com.example.demo.mapper;
+
+import com.example.demo.moudle.Student;
+
+public interface StudentMapper {
+
+    public int insertStudent(Student student);
+}
---
+++ src/main/java/com/example/demo/moudle/Student.java
@@ -0,0 +1,31 @@
+package com.example.demo.moudle;
+
+public class Student {
+    private int id;
+    private String name;
+    private String classId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassId() {
+        return classId;
+    }
+
+    public void setClassId(String classId) {
+        this.classId = classId;
+    }
+}
