ee074fd0c16261718e56fdafb6927a975cc6b375
testgit <testgit>
2022-06-18 10:57:58 +0800

    gfrsd

---
+++ src/main/java/com/example/demo/util/HttpServer.java
@@ -0,0 +1,143 @@
+package com.example.demo.util;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpHeaders.Values;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+
+public class HttpServer {
+    public void start(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            // server端发送的是httpResponse，所以要使用HttpResponseEncoder进行编码
+                            ch.pipeline().addLast(new HttpResponseEncoder());
+                            // server端接收到的是httpRequest，所以要使用HttpRequestDecoder进行解码
+                            ch.pipeline().addLast(new HttpRequestDecoder());
+                            ch.pipeline().addLast(new HttpServerInboundHandler());
+                        }
+                    }).option(ChannelOption.SO_BACKLOG, 128) // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            ChannelFuture f = b.bind(port).sync(); // (7)
+
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = new HttpServer();
+        server.start(8000);
+    }
+}
+class HttpServerInboundHandler extends ChannelInboundHandlerAdapter {
+    //private static Logger    logger    = LoggerFactory.getLogger(HttpServerInboundHandler.class);
+    private ByteBufToBytes reader;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof HttpRequest) {
+            HttpRequest request = (HttpRequest) msg;
+            System.out.println("messageType:" + request.headers().get("messageType"));
+            System.out.println("businessType:" + request.headers().get("businessType"));
+            if (HttpHeaders.isContentLengthSet(request)) {
+                reader = new ByteBufToBytes((int) HttpHeaders.getContentLength(request));
+            }
+        }
+
+        if (msg instanceof HttpContent) {
+            HttpContent httpContent = (HttpContent) msg;
+            ByteBuf content = httpContent.content();
+            reader.reading(content);
+            content.release();
+
+            if (reader.isEnd()) {
+                String resultStr = new String(reader.readFull());
+                System.out.println("Client said:" + resultStr);
+
+                FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer("I am ok"
+                        .getBytes()));
+                response.headers().set(CONTENT_TYPE, "text/plain");
+                response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+                response.headers().set(CONNECTION, Values.KEEP_ALIVE);
+                ctx.write(response);
+                ctx.flush();
+            }
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        //logger.info("HttpServerInboundHandler.channelReadComplete");
+        ctx.flush();
+    }
+
+}
+class ByteBufToBytes {
+    private ByteBuf    temp;
+
+    private boolean    end    = true;
+
+    public ByteBufToBytes(int length) {
+        temp = Unpooled.buffer(length);
+    }
+
+    public void reading(ByteBuf datas) {
+        datas.readBytes(temp, datas.readableBytes());
+        if (this.temp.writableBytes() != 0) {
+            end = false;
+        } else {
+            end = true;
+        }
+    }
+
+    public boolean isEnd() {
+        return end;
+    }
+
+    public byte[] readFull() {
+        if (end) {
+            byte[] contentByte = new byte[this.temp.readableBytes()];
+            this.temp.readBytes(contentByte);
+            this.temp.release();
+            return contentByte;
+        } else {
+            return null;
+        }
+    }
+
+    public byte[] read(ByteBuf datas) {
+        byte[] bytes = new byte[datas.readableBytes()];
+        datas.readBytes(bytes);
+        return bytes;
+    }
+}
